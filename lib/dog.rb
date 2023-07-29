6be30269fcf02a319a30aa5bd4b6e1f4ba7a9079
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-07-29 09:00:35 -0400

    Lab Solution

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,20 @@
-class Dog
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/test.sqlite3"
+)
+
+# set up a database connection
+db = ActiveRecord::Base.connection
+
+db.execute("DROP TABLE IF EXISTS dogs")
+db.execute("CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY, 
+    name TEXT, 
+    breed TEXT
+    );")
+
+class Dog < ActiveRecord::Base
 end
